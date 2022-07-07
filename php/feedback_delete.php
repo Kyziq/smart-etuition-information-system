9677f1e7361965b4853352e00fa0b68d3e5295bc
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-07-07 23:08:38 +0800

    Removed popup

--- php/feedback_delete.php
+++ php/feedback_delete.php
@@ -14,16 +14,18 @@ if (isset($_SESSION['userID']) && $_SESSION['userLevel'] == 1) {
         $res = mysqli_query($con, $q);
 
         // Success popup
+        /*
         echo
         "
             <script>
                 alert('Feedback deletion succesful!');
                 window.location.href='feedback.php';
             </script>
-        ";
+        ";*/
+        header("Location: feedback.php");
 
         // Clear results and close the connection
-        mysqli_free_result($res);
+        // mysqli_free_result($res);
         mysqli_close($con);
     } else
         header("Location: feedback.php");

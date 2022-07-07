8465fbb8f3d46e5f69852517d9b01a90f5487aa8
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-07-08 02:48:31 +0800

    Updated mobile login

--- mobile/login.php
+++ mobile/login.php
@@ -1,16 +1,37 @@
 <?php
-include "dbconn.php";
-$userUname=$_POST['userUname'];
-$userPassw=$_POST['userPassw'];
+// Inialize session
+session_start();
 
-$q = "SELECT * FROM user WHERE userName='$userName' AND userPassw='$userPassw'";
-$result = mysqli_query($con, $q);
-$rows = mysqli_num_rows($result);
+// Include database connection settings
+include_once 'dbcon.php';
 
-if ($rows>0) {
-	echo "Login Successful!";
+// capture values from HTML form
+$userUname = $_POST['userUname'];
+$userPassw = $_POST['userPassw'];
+
+$sql = "SELECT userUname, userPassw, userLevel FROM user WHERE userUname= '$userUname' AND userPassw= '$userPassw'";
+$query = mysqli_query($con, $sql);
+$row = mysqli_num_rows($query);
+
+if ($row == 0) {
+    // Jump to index wrong page
+    echo "No data received";
+} else {
+    $r = mysqli_fetch_assoc($query);
+    $userUname = $r['userUname'];
+    //$userPassw= $r['userPassw'];
+    $userLevel = $r['userLevel'];
+
+    if ($userLevel == 1)
+        echo "1";
+    else if ($userLevel == 2)
+        echo "2";
+    else if ($userLevel == 3)
+        echo "3";
 }
-else {
-	echo "Login Unsuccessful:(";
-}
-?>
+
+//echo $userUname;
+//echo "<br>";
+//echo $userPassw;
+
+mysqli_close($con);

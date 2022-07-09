38d8386f81195fdfdba55ab60c44f86b6a584ecb
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-07-09 10:22:57 +0800

    Update

--- mobile/register.php
+++ mobile/register.php
@@ -1,29 +1,30 @@
 <?php
-include "dbcon.php";
-$userID=$_POST['userID'];
-$userUname=$_POST['userUname'];
-$userPassw=$_POST['userPassw'];
-$userName=$_POST['userName'];
-$userPhone=$_POST['userPhone'];
-$userEmail=$_POST['userEmail'];
-$userAddress=$_POST['userAddress'];
-$userGender=$_POST['userGender'];
+// Include database connection settings
+include_once 'dbcon.php';
 
+$userID = $_POST['userID'];
+$userUname = $_POST['userUname'];
+$userPassw = $_POST['userPassw'];
+$userName = $_POST['userName'];
+$userPhone = $_POST['userPhone'];
+$userEmail = $_POST['userEmail'];
+$userAddress = $_POST['userAddress'];
+$userGender = $_POST['userGender'];
 
 $q = "SELECT * FROM user WHERE userUname='userUname'";
 $result = mysqli_query($con, $q);
 $rows = mysqli_num_rows($result);
 
 if ($rows > 0) {
-	echo "Data is already saved";
-}
-else {
-	$q = "INSERT INTO user(userID, userUname, userPassw, userName, userPhone, userEmail, userAddress, userGender) VALUES 
+    echo "Data is already saved";
+} else {
+    $q = "INSERT INTO user(userID, userUname, userPassw, userName, userPhone, userEmail, userAddress, userGender) VALUES 
 	('$userID', '$userUname', '$userPassw', '$userName', '$userPhone', '$userEmail', '$userAddress', '$userGender')";
-	$input = mysqli_query($con, $q);
-	if ($input) {
-		echo "Registration Successful!";
-	}
+    $input = mysqli_query($con, $q);
+
+    if ($input) {
+        echo "Registration successful!\nPlease wait a few seconds.";
+    }
 }
 /*
 $userBirthdate=$_POST['userBirthdate'];
@@ -31,4 +32,3 @@ $userBirthdate=$_POST['userBirthdate'];
 , userBirthdate
 , '$userBirthdate'
 */
-?>

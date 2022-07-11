d6fd214e5d090de3d4ce6e1066f2eef3b2df0cdb
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-07-11 20:18:01 +0800

    Data export coding files updated

--- php/export/feedback_details.php
+++ php/export/feedback_details.php
@@ -1,36 +1,44 @@
 <?php
+session_start();
 // Export data to CSV
-if (isset($_GET['exportFeedbackDetails'])) {
-    include_once '../dbcon.php';
-    // Fetch records from database 
-    $query = $con->query("SELECT * FROM feedback");
+if (isset($_SESSION['userID']) && $_SESSION['userLevel'] == 1) {
+    if (isset($_GET['exportFeedbackDetails'])) {
+        include_once '../dbcon.php';
+        // Fetch records from database 
+        $query = $con->query("SELECT * FROM feedback");
 
-    if ($query->num_rows > 0) {
-        $delimiter = ",";
-        $filename = "feedbackList " . date('Y-m-d') . ".csv";
+        if ($query->num_rows > 0) {
+            $delimiter = ",";
+            $filename = "feedbackList " . date('Y-m-d') . ".csv";
 
-        // Create a file pointer 
-        $f = fopen('php://memory', 'w');
+            // Create a file pointer 
+            $f = fopen('php://memory', 'w');
 
-        // Set column headers 
-        $fields = array('fbID', 'fbTitle', 'fbComment', 'fbDate');
-        fputcsv($f, $fields, $delimiter);
+            // Set column headers 
+            $fields = array('fbID', 'fbTitle', 'fbComment', 'fbDate');
+            fputcsv($f, $fields, $delimiter);
 
-        // Output each row of the data, format line as csv and write to file pointer 
-        while ($row = $query->fetch_assoc()) {
-            $lineData = array($row['fbID'], $row['fbTitle'], $row['fbComment'], $row['fbDate']);
-            fputcsv($f, $lineData, $delimiter);
-        }
+            // Output each row of the data, format line as csv and write to file pointer 
+            while ($row = $query->fetch_assoc()) {
+                $lineData = array($row['fbID'], $row['fbTitle'], $row['fbComment'], $row['fbDate']);
+                fputcsv($f, $lineData, $delimiter);
+            }
 
-        // Move back to beginning of file 
-        fseek($f, 0);
+            // Move back to beginning of file 
+            fseek($f, 0);
 
-        // Set headers to download file rather than displayed 
-        header('Content-Type: text/csv');
-        header('Content-Disposition: attachment; filename="' . $filename . '";');
+            // Set headers to download file rather than displayed 
+            header('Content-Type: text/csv');
+            header('Content-Disposition: attachment; filename="' . $filename . '";');
 
-        //output all remaining data on a file pointer 
-        fpassthru($f);
-    }
+            //output all remaining data on a file pointer 
+            fpassthru($f);
+        } else
+            echo    "<script>
+                        alert('No data available to export.');
+                        window.location.href='../feedback.php';
+                    </script>";
+    } else
+        header("Location: ../login.php");
     exit;
 }
--- php/export/tutor_details.php
+++ php/export/tutor_details.php
@@ -1,37 +1,45 @@
 <?php
+session_start();
 // Export data to CSV
-if (isset($_GET['exportTutorDetails'])) {
-    include_once '../dbcon.php';
-    // Fetch records from database 
-    $query = $con->query("SELECT * FROM user WHERE userLevel='2'");
+if (isset($_SESSION['userID']) && $_SESSION['userLevel'] == 1) {
+    if (isset($_GET['exportTutorDetails'])) {
+        include_once '../dbcon.php';
+        // Fetch records from database 
+        $query = $con->query("SELECT * FROM user WHERE userLevel='2'");
 
-    if ($query->num_rows > 0) {
-        $delimiter = ",";
-        $filename = "tutorList " . date('Y-m-d') . ".csv";
+        if ($query->num_rows > 0) {
+            $delimiter = ",";
+            $filename = "tutorList " . date('Y-m-d') . ".csv";
 
-        // Create a file pointer 
-        $f = fopen('php://memory', 'w');
+            // Create a file pointer 
+            $f = fopen('php://memory', 'w');
 
-        // Set column headers 
-        $fields = array('userID', 'userName', 'userGender', 'userPhone', 'userEmail', 'userBirthdate', 'userAddress');
-        fputcsv($f, $fields, $delimiter);
+            // Set column headers 
+            $fields = array('userID', 'userName', 'userGender', 'userPhone', 'userEmail', 'userBirthdate', 'userAddress');
+            fputcsv($f, $fields, $delimiter);
 
-        // Output each row of the data, format line as csv and write to file pointer 
-        while ($row = $query->fetch_assoc()) {
-            $status = ($row['userGender'] == 1) ? 'Male' : 'Gender';
-            $lineData = array($row['userID'], $row['userName'], $status, $row['userPhone'], $row['userEmail'], $row['userBirthdate'], $row['userAddress']);
-            fputcsv($f, $lineData, $delimiter);
-        }
+            // Output each row of the data, format line as csv and write to file pointer 
+            while ($row = $query->fetch_assoc()) {
+                $status = ($row['userGender'] == 1) ? 'Male' : 'Gender';
+                $lineData = array($row['userID'], $row['userName'], $status, $row['userPhone'], $row['userEmail'], $row['userBirthdate'], $row['userAddress']);
+                fputcsv($f, $lineData, $delimiter);
+            }
 
-        // Move back to beginning of file 
-        fseek($f, 0);
+            // Move back to beginning of file 
+            fseek($f, 0);
 
-        // Set headers to download file rather than displayed 
-        header('Content-Type: text/csv');
-        header('Content-Disposition: attachment; filename="' . $filename . '";');
+            // Set headers to download file rather than displayed 
+            header('Content-Type: text/csv');
+            header('Content-Disposition: attachment; filename="' . $filename . '";');
 
-        //output all remaining data on a file pointer 
-        fpassthru($f);
-    }
+            //output all remaining data on a file pointer 
+            fpassthru($f);
+        } else
+            echo    "<script>
+                        alert('No data available to export.');
+                        window.location.href='../manage_tutor.php';
+                    </script>";
+    } else
+        header("Location: ../login.php");
     exit;
 }
